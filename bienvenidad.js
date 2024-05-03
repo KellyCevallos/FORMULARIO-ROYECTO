f2b408edc37c66a7f1f1387377eb3e777663e190
KellyCevallos <132920660+KellyCevallos@users.noreply.github.com>
2024-05-03 14:35:33 -0500

    Add files via upload

---
+++ bienvenidad.js
@@ -0,0 +1,4 @@
+
+setTimeout(function() {
+    window.location.href = 'bienvenida.html';
+}, 3000); 
---
+++ procesar_inicio.js
@@ -0,0 +1,9 @@
+
+var inicioSesionExitoso = true;
+
+if (inicioSesionExitoso) {
+    
+    setTimeout(function() {
+        window.location.href = 'bienvenida.html';
+    }, 2000); 
+}
